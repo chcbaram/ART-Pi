78b70ec68321a06f42b121496fea9d783b7582c1
Baram <chcbaram@gmail.com>
2021-08-08 12:15:08 +0900

    CubeIDE 1.7.0에서 오류 수정 추가.

--- art_pi_el_fw/src/ap/Dev_Inf.c
+++ art_pi_el_fw/src/ap/Dev_Inf.c
@@ -14,7 +14,7 @@
 #if defined (__ICCARM__)
 __root struct StorageInfo const StorageInfo  =  {
 #else
-struct StorageInfo const StorageInfo  =  {
+__attribute__((section(".Dev_info"))) struct StorageInfo const StorageInfo  =  {
 #endif
    "W25Q128JV_H750-ART-PI",               // Device Name + EVAL Borad name
    SPI_FLASH,                             // Device Type
@@ -23,6 +23,6 @@ struct StorageInfo const StorageInfo  =  {
    0x100,                                 // Programming Page Size 256Bytes
    0xFF,                                  // Initial Content of Erased Memory
 // Specify Size and Address of Sectors (view example below)
-   0x00000100, 0x00010000,                // Sector Num : 256 ,Sector Size: 64KBytes
-   0x00000000, 0x00000000,
+   {{0x00000100, 0x00010000,},            // Sector Num : 256 ,Sector Size: 64KBytes
+    {0x00000000, 0x00000000,}}
 };
